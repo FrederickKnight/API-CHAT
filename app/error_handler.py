7d36b86caa3e5bf77c72ac65f5bb8bb7fdeb2d01
Frederick Knight <infisefir@gmail.com>
2025-09-09 14:06:51 -0600

    Refactor: Delete raise error

--- app/error_handler.py
+++ app/error_handler.py
@@ -19,13 +19,11 @@ def _get_schema_error(error,message,details):
 def register_error_handlers(app):
     @app.errorhandler(AppError)
     def handle_app_error(error):
-        raise error
         schema_error = _get_schema_error(error.error,error.message,error.details)
         return Response(response=json.dumps(schema_error), status=400, mimetype="application/json")
 
     @app.errorhandler(IntegrityError)
     def handle_integrity_error(error):
-        raise error
         schema_error = _get_schema_error(
             error="Integrity Error",
             message="Invalid given data or parameters",
@@ -35,13 +33,11 @@ def register_error_handlers(app):
 
     @app.errorhandler(Exception)
     def handle_generic_error(error):
-        raise error
         schema_error = _get_schema_error(error="Unhandled Error",message="Something went wrong",details=str(error))
         return Response(response=json.dumps(schema_error), status=500, mimetype="application/json") 
     
     
     @app.errorhandler(TypeError)
     def handle_type_error(error):
-        raise error
         schema_error = _get_schema_error(error="Type Error",message="Invalid type given",details=str(error))
         return Response(response=json.dumps(schema_error), status=400, mimetype="application/json")
